367ed8b555dd6fdc4cf4d359a6ca54a04f061ab1
Luis Antonio Momm Duarte <luismomm@gmail.com>
2022-08-30 19:29:22 -0300

    p2

---
+++ p2/heap_sort.py
@@ -0,0 +1,50 @@
+def main():
+    array = [7, 8, 3, 2, 5, 2, 1, 4, 3, 0]
+
+    heapSort(array)
+
+    N = len(array)
+    for i in range(N):
+        print("%d" % array[i], end=" ")
+
+
+def open_file(filepath: str):
+    names = []
+    with open(filepath, "r") as f:
+        f.readline()
+        for line in f.readlines():
+            current_name = [x for x in line.split(",")]
+            names.append(current_name[0].strip('"'))
+    return sorted(names)
+
+
+def heapify(array, N, i):
+    largest = i
+    left = 2*i + 1
+    right = 2*i + 2
+
+    if left < N and array[left] > array[largest]:
+        largest = left
+
+    if right < N and array[right] > array[largest]:
+        largest = right
+
+    if largest != i:
+        array[i], array[largest] = array[largest], array[i]
+
+        heapify(array, N, largest)
+
+
+def heapSort(array):
+    N = len(array)
+
+    for i in range(N//2 - 1, -1, -1):
+        heapify(array, N, 0)
+
+    for i in range(N-1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        heapify(array, i, 0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ p2/max_subarray_sum.py
@@ -0,0 +1,46 @@
+from math import inf
+
+
+def maxSubArraySum(array, left, right):
+    if (left > right):
+        return -inf
+
+    if (left == right):
+        return array[left]
+
+    middle = (left + right) // 2
+
+    return max(maxSubArraySum(array, left, middle-1),
+               maxSubArraySum(array, middle+1, right),
+               maxCrossingSum(array, left, middle, right))
+
+
+def maxCrossingSum(array, left, middle, right):
+    sum = 0
+    left_sum = -inf
+
+    for i in range(middle, left-1, -1):
+        sum = sum + array[i]
+
+        if (sum > left_sum):
+            left_sum = sum
+
+    sum = 0
+    right_sum = -1000
+    for i in range(middle, right + 1):
+        sum = sum + array[i]
+
+        if (sum > right_sum):
+            right_sum = sum
+
+    return max(left_sum + right_sum - array[middle], left_sum, right_sum)
+
+
+def main():
+    array = [-4, 5, 7, -6, 10, -15, 3]
+
+    print(maxSubArraySum(array, 0, len(array)-1))
+
+
+if __name__ == "__main__":
+    main()
