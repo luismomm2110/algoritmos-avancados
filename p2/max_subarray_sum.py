dd3a71d34897b0ed362082fc22c0876f8a474796
Luis Antonio Momm Duarte <luismomm@gmail.com>
2022-09-02 21:07:24 -0300

    explicacao heap sort e max subarray

--- p2/max_subarray_sum.py
+++ p2/max_subarray_sum.py
@@ -10,6 +10,7 @@ def maxSubArraySum(array, left, right):
 
     middle = (left + right) // 2
 
+    # escolhe o maximo entre eles
     return max(maxSubArraySum(array, left, middle-1),
                maxSubArraySum(array, middle+1, right),
                maxCrossingSum(array, left, middle, right))
@@ -19,6 +20,7 @@ def maxCrossingSum(array, left, middle, right):
     sum = 0
     left_sum = -inf
 
+    # acha a soma contigua, se diminuir é porque nao é uma soma contigua, entao ignora
     for i in range(middle, left-1, -1):
         sum = sum + array[i]
 
@@ -26,20 +28,24 @@ def maxCrossingSum(array, left, middle, right):
             left_sum = sum
 
     sum = 0
-    right_sum = -1000
+    right_sum = -inf
+    # acha a soma contigua, se diminuir é porque nao é uma soma contigua, entao ignora
     for i in range(middle, right + 1):
         sum = sum + array[i]
 
         if (sum > right_sum):
             right_sum = sum
 
+    # tira o meio para nao somar duas vezes
     return max(left_sum + right_sum - array[middle], left_sum, right_sum)
+
+# Tempo de complexidade O(n(logn)) pelo metodo mestre https://www.enjoyalgorithms.com/blog/maximum-subarray-sum
 
 
 def main():
     array = [-4, 5, 7, -6, 10, -15, 3]
-
-    print(maxSubArraySum(array, 0, len(array)-1))
+    result = maxSubArraySum(array, 0, len(array)-1)
+    assert(result == 16)
 
 
 if __name__ == "__main__":
